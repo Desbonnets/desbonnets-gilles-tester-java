f3504cd68b580c2650a02f30b437255a08896db5
Gilles DESBONNETS <gillesdesbonnets@gmail.com>
2024-10-25 15:06:04 +0200

    fix message de remise

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -17,7 +17,6 @@ public class FareCalculatorService {
         long inHour = ticket.getInTime().getTime();
         long outHour = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
         double duration = (double) (outHour - inHour) / (1000 * 60 * 60);
 
         if(duration <= 0.5){

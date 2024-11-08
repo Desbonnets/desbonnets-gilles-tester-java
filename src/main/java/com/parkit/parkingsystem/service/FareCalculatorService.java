53b65eff3e280f3b25bfb908482c078a32216353
Gilles DESBONNETS <gillesdesbonnets@gmail.com>
2024-11-08 11:17:28 +0100

    fix arrondi + final test

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,6 +3,9 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket) {
@@ -26,11 +29,15 @@ public class FareCalculatorService {
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR * (discount ? 0.95 : 1));
+                BigDecimal bd = new BigDecimal(duration * Fare.CAR_RATE_PER_HOUR * (discount ? 0.95 : 1));
+                bd = bd.setScale(3, RoundingMode.HALF_UP);
+                ticket.setPrice(bd.doubleValue());
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR * (discount ? 0.95 : 1));
+                BigDecimal bd = new BigDecimal(duration * Fare.BIKE_RATE_PER_HOUR * (discount ? 0.95 : 1));
+                bd = bd.setScale(3, RoundingMode.HALF_UP);
+                ticket.setPrice(bd.doubleValue());
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,13 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +17,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,6 +32,7 @@ public class ParkingDataBaseIT {
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
+    private String vehicleRegNumber = "ABCDEF";
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
@@ -38,7 +49,7 @@ public class ParkingDataBaseIT {
     @BeforeEach
     private void setUpPerTest() throws Exception {
         when(inputReaderUtil.readSelection()).thenReturn(1);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn(vehicleRegNumber);
         dataBasePrepareService.clearDataBaseEntries();
     }
 
@@ -51,15 +62,58 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+
+        Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
+        assertNotNull(ticket, "Le ticket ne doit pas être null");
+        assertEquals(vehicleRegNumber, ticket.getVehicleRegNumber(), "Le numéro de véhicule doit correspondre");
+
+        ParkingSpot parkingSpot = ticket.getParkingSpot();
+        assertFalse(parkingSpot.isAvailable(), "Le parking doit être marqué comme non disponible");
     }
 
     @Test
     public void testParkingLotExit(){
         testParkingACar();
+
+        Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );
+        ticket.setInTime(inTime);
+        ticketDAO.saveTicket(ticket);
+
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+
+        ticket = ticketDAO.getTicket(vehicleRegNumber);
+
+        System.out.println(ticket.getOutTime());
+
+        assertNotNull(ticket.getOutTime(), "L'heure de sortie ne doit pas être null");
+        assertTrue(ticket.getPrice() > 0, "Le tarif doit être supérieur à 0");
+    }
+
+    @Test
+    public void testParkingLotExitRecurringUser() {
+
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
+
+        Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
+        ticket.setParkingSpot(new ParkingSpot(1, ParkingType.CAR, false));
+        ticket.setInTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000))); // Une heure dans le passé
+        ticketDAO.saveTicket(ticket);
+
+        parkingService.processExitingVehicle();
+
+        ticket = ticketDAO.getTicket(vehicleRegNumber);
+
+        // Vérifier que le prix a été calculé avec la remise de 5%
+        BigDecimal bd = new BigDecimal(Fare.CAR_RATE_PER_HOUR * 0.95);
+        bd = bd.setScale(3, RoundingMode.HALF_UP);
+        double expectedPrice = bd.doubleValue();
+        double actualPrice = ticket.getPrice();
+
+        assertEquals(expectedPrice, actualPrice);
     }
 
 }
